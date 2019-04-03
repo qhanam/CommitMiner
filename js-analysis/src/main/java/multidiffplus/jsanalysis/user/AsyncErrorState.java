551f3d6668f941743c335cd74792c60f7054e5f6
Quinn Hanam <qhanam@gmail.com>
2019-04-03 14:20:24 -0700

    store param names to lookup during async error handling checker

--- js-analysis/src/main/java/multidiffplus/jsanalysis/user/AsyncErrorState.java
+++ js-analysis/src/main/java/multidiffplus/jsanalysis/user/AsyncErrorState.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.mozilla.javascript.ast.AstNode;
 import org.mozilla.javascript.ast.FunctionCall;
 import org.mozilla.javascript.ast.FunctionNode;
@@ -18,6 +19,7 @@ import multidiffplus.jsanalysis.abstractdomain.BValue;
 import multidiffplus.jsanalysis.abstractdomain.Change;
 import multidiffplus.jsanalysis.abstractdomain.Criterion;
 import multidiffplus.jsanalysis.abstractdomain.Str;
+import multidiffplus.jsanalysis.abstractdomain.Variable;
 import multidiffplus.jsanalysis.flow.JavaScriptAnalysisState;
 import multidiffplus.jsanalysis.interpreter.ExpEval;
 
@@ -28,17 +30,35 @@ public class AsyncErrorState implements IUserState {
      */
     private Map<FunctionCall, Criterion> asyncCallsites;
 
+    /**
+     * The error from the asynchronous function.
+     */
+    private Pair<Variable, Criterion> error;
+
+    /**
+     * The return values from the asynchronous function.
+     */
+    private Map<Variable, Criterion> returnValues;
+
     private AsyncErrorState() {
 	this.asyncCallsites = new HashMap<>();
+	this.error = null;
+	this.returnValues = new HashMap<>();
     }
 
-    private AsyncErrorState(FunctionCall fc) {
+    private AsyncErrorState(FunctionCall fc, Pair<Variable, Criterion> error,
+	    Map<Variable, Criterion> returnValues) {
 	this.asyncCallsites = new HashMap<>();
-	this.asyncCallsites.put(fc, Criterion.of(fc, Criterion.Type.ASYNC_ERROR));
+	this.asyncCallsites.put(fc, Criterion.of(fc, Criterion.Type.ASYNC_ERROR_CALL_SITE));
+	this.error = error;
+	this.returnValues = returnValues;
     }
 
-    private AsyncErrorState(Map<FunctionCall, Criterion> asyncCallsites) {
+    private AsyncErrorState(Map<FunctionCall, Criterion> asyncCallsites,
+	    Pair<Variable, Criterion> error, Map<Variable, Criterion> returnValues) {
 	this.asyncCallsites = asyncCallsites;
+	this.error = error;
+	this.returnValues = returnValues;
     }
 
     @Override
@@ -51,7 +71,7 @@ public class AsyncErrorState implements IUserState {
 	// Look up the BValue of the parameter e === null || e !== null
 	AstNode node = (AstNode) statement;
 	ExpEval eval = ((JavaScriptAnalysisState) builtin).getExpressionEvaluator();
-	BValue val = eval.resolveValue(new Name(0, "e"));
+	BValue val = eval.resolveValue(new Name(0, error.getKey().name));
 	if (val == null) {
 	    return this;
 	}
@@ -63,6 +83,7 @@ public class AsyncErrorState implements IUserState {
 	    for (Criterion criterion : asyncCallsites.values()) {
 		node.addDependency(criterion.getType().toString(), criterion.getId());
 	    }
+	    node.addDependency(error.getValue().getType().toString(), error.getValue().getId());
 	}
 
 	return this;
@@ -82,12 +103,28 @@ public class AsyncErrorState implements IUserState {
     @Override
     public IUserState initializeCallback(IBuiltinState builtin, ClassifiedASTNode callSite,
 	    CFG function) {
+	JavaScriptAnalysisState state = (JavaScriptAnalysisState) builtin;
 	FunctionCall fc = (FunctionCall) callSite;
+
 	// TODO: Resolve the target and callback function to see if either have changed.
-	// TODO: Identify the name of the callback parameter (ie. the first parameter).
+
 	if (Change.test(fc) || Change.testU(fc.getTarget())) {
 	    // The call site is new, or the target has changed.
-	    return new AsyncErrorState(fc);
+	    FunctionNode f = (FunctionNode) function.getEntryNode().getStatement();
+	    Pair<Variable, Criterion> error = null;
+	    Map<Variable, Criterion> params = new HashMap<>();
+	    int i = 0;
+	    for (AstNode param : f.getParams()) {
+		if (i == 0) {
+		    Variable v = state.getUnderlyingState().env.apply((Name) param);
+		    error = Pair.of(v, Criterion.of(param, Criterion.Type.ASYNC_ERROR_EPARAM));
+		} else {
+		    Variable v = state.getUnderlyingState().env.apply((Name) param);
+		    params.put(v, Criterion.of(param, Criterion.Type.ASYNC_ERROR_VPARAM));
+		}
+		i++;
+	    }
+	    return new AsyncErrorState(fc, error, params);
 	} else {
 	    // The call site is not impacted by the change.
 	    return new AsyncErrorState();
@@ -99,7 +136,7 @@ public class AsyncErrorState implements IUserState {
 	Map<FunctionCall, Criterion> newAsyncCallsites = new HashMap<>();
 	newAsyncCallsites.putAll(this.asyncCallsites);
 	newAsyncCallsites.putAll(((AsyncErrorState) that).asyncCallsites);
-	return new AsyncErrorState(newAsyncCallsites);
+	return new AsyncErrorState(newAsyncCallsites, error, returnValues);
     }
 
     @Override
